c8d0a384cdb2a368810c6b9d1be6d78c40d67bd6
Ali Cataltepe <ali@cataltepe.com>
2013-11-09 20:18:32 +0200

    first commit

---
+++ ROS-LaserScan-Obstruction-Detection.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# Copyright 2013 Ali Cataltepe, licensed under Creative Commons 3.0.
+# This library contains functions which allow a robot using the LaserScan
+# message format for its sensors to detect obstacles to the front, left and
+# right.
+import roslib; roslib.load_manifest('Delete and replace with package name')
+import rospy
+from sensor_msgs.msg import LaserScan
+import config
+
+# Analyzes data sent from the sensors function, and cycles through different
+# portions of it in order to check for obstacles. Afterwards, a list
+# containing letters, the presence of which depends on the presence of an
+# obstacle to the side represented by the adorementioned letter. The robot can
+# then make decisions according to the presence or lack thereof of obstacles.
+def obstrdetect(data):  
+	obstr = []
+	# You can configure the index numbers for the slices which will be
+	# scanned in config.py
+	leftend = 684
+	for x in data.ranges[config.frontbegin:config.frontend]:
+		if x < config.frontdistance:
+			print 'Obstruction to the front!'
+			obstr.append('f')
+			break
+	for y in data.ranges[config.rightbegin:config.rightend]:
+		if y < config.rightdistance:
+			print 'Obstruction to the right!'
+			obstr.append('r')
+			break
+	for z in data.ranges [config.leftbegin:config.leftend]:
+		if z < config.leftdistance:
+			print 'Obstruction to the left!'
+			obstr.append('l')
+			break
+	return obstr
+
+# Periodically calls obstrdetect with the sensor data as arguments.
+def sensors():
+	rospy.init_node('sensub')	
+	rospy.Subscriber('Delete and replace with the name of the topic your robot is publishing its sensor data to', LaserScan, obstrdetect)
+	rospy.spin()
---
+++ config.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# Enter the values for the boundaries of the slices denoting the front, left
+# and right of your robot, and the minimum distance for the laser scans to be  # considered obstructions.
+frontbegin = 227
+frontend = 454
+rightbegin = 0
+rightend = 226
+leftbegin = 454
+leftend = 684
+frontdistance = 1.0
+rightdistance = 1.0
+leftdistance = 1.0
