4b2fa10c0ef2c27b3787d525163693e2e0df64b2
Ali Cataltepe <ali@cataltepe.com>
2013-11-30 19:26:41 +0200

    added variables for the front and left diagonal directions for the robot to the ObstructionList message format, and changed config.py and the main script to define these variables for the message too

--- ROS_LaserScan_Obstruction_Detection.py
+++ ROS_LaserScan_Obstruction_Detection.py
@@ -9,7 +9,7 @@ import rospy
 from sensor_msgs.msg import LaserScan
 from '''YOURPACKAGENAME'''.msg import ObstructionList
 
-# Analyzes data sent from the sensors function, and cycles through different
+# Analyzes data sent from the sensors() function, and cycles through different
 # portions of it in order to check for obstacles. Adds boolean values to the
 # obstr dataset. These values denote the presence or absence of an obstacle in
 # a given direction. A value can be accessed by typing obstr.name (or whichever
@@ -17,22 +17,32 @@ from '''YOURPACKAGENAME'''.msg import ObstructionList
 # the presence or absence of an obstacle to the robot's front.
 def obstrdetect(data):
 	obstr = ObstructionList()
-	obstr.frontobstr = False; obstr.rightobstr = False; obstr.leftobstr = False;
+	obstr.frontobstr = False; obstr.rightobstr = False; obstr.leftobstr = False; obstr.frontrightobstr = False; obstr.frontleftobstr = False;
 	# You can configure the index numbers for the slices which will be
 	# scanned in config.py.
 	for x in data.ranges[config.frontbegin:config.frontend]:
 		if x < config.frontdistance:
-			obstr.frontobstr = True
+			obstr.frontobstr = True;
 			break
 	for y in data.ranges[config.rightbegin:config.rightend]:
 		if y < config.rightdistance:
-			obstr.rightobstr = True
+			obstr.rightobstr = True;
 			break
-	for z in data.ranges [config.leftbegin:config.leftend]:
+	for z in data.ranges[config.leftbegin:config.leftend]:
 		if z < config.leftdistance:
-			obstr.leftobstr = True
+			obstr.leftobstr = True;
 			break
+    for t in data.ranges[config.frontrightbegin:config.frontrightend]:
+        if t < config.frontrightdistance:
+            obstr.frontrightobstr = True;
+            break
+    for v in data.ranges[config.frontleftbegin:config.frontleftend]:
+        if v < config.frontleftdistance:
+            obstr.frontleftobstr = True;
+            break
 	obstrpub = rospy.Publisher('obstructions', ObstructionList)
+    timeofpub = rospy.time.now()
+    obstr.pubtime = timeofpub;
 	obstrpub.publish(obstr)
 
 # Calls obstrdetect with the sensor data as arguments.
--- config.py
+++ config.py
@@ -2,19 +2,24 @@
 # Assign the name of your package to the 'packagename' variable.
 # Assign the name of the node your laser scanner transmits data from to the
 # 'subscribednode' variable.
-# Assign your package's unquoted .msg file name (yourpackagename.msg) to the
-# 'messagefile' variable.
-# Enter the values for the boundaries of the slices denoting the front, left
-# and right of your robot, and the minimum distance (m) for the laser scans
+# Enter the values for the boundaries of the slices denoting the front,
+# left, right, front-left and front-right of your robot, and the minimum
+# distance (m) for the laser scans
 # to be considered obstructions.
 packagename = 'your package name here'
 subscribednode = 'your subscribed topic name here'
-frontbegin = 227
-frontend = 454
+frontbegin = 260
+frontend = 390
 rightbegin = 0
-rightend = 226
-leftbegin = 454
+rightend = 130
+leftbegin = 550
 leftend = 684
+frontrightbegin = 130
+frontrightend = 260
+frontleftbegin = 420
+frontleftend = 550
 frontdistance = 1.0
 rightdistance = 1.0
 leftdistance = 1.0
+frontleftdistance = 1.0
+frontrightdistance = 1.0
